f617be1dfa21712864a6d609c21ad23837cfc898
Scott McFarland <scottm36@gmail.com>
2025-01-30 23:19:20 -0500

    Added another test

--- cypress/e2e/react-test/counter.cy.ts
+++ cypress/e2e/react-test/counter.cy.ts
@@ -12,5 +12,10 @@ describe('Counter and reset test', () => {
         cy.get('button[data-cy="reset"]').click();
         cy.get('button[data-cy="increment"]').should('have.text', 'count is 0');
     });
+
+    it('should have the correct title', () => {
+        cy.visit('http://localhost:5173');
+        cy.get('h1[data-cy="title"]').should('have.text', 'Vite + React');
+    });
 });
 
--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 data-cy="title">Vite + React</h1>
       <div className="card">
         <button data-cy="increment" onClick={() => setCount((count) => count + 1)}>
           count is {count}
