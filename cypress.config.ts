7f73c3616499ae80f5cf4fbe97e62db4c31d792f
Scott McFarland <scottm36@gmail.com>
2025-01-31 19:19:52 -0500

    Removed default code, starting from blank dark page.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  video: true,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/react-test/counter.cy.ts
+++ cypress/e2e/react-test/counter.cy.ts
@@ -1,19 +1,19 @@
 import cypress from "cypress";
 
 describe('Counter and reset test', () => {
-    it('should increment the counter', () => {
+    it.skip('should increment the counter', () => {
         cy.visit('http://localhost:5173');
         cy.get('button[data-cy="increment"]').click();
         cy.get('button[data-cy="increment"]').should('have.text', 'count is 1');
     });
 
-    it('should reset the counter', () => {
+    it.skip('should reset the counter', () => {
         cy.visit('http://localhost:5173');
         cy.get('button[data-cy="reset"]').click();
         cy.get('button[data-cy="increment"]').should('have.text', 'count is 0');
     });
 
-    it('should have the correct title', () => {
+    it.skip('should have the correct title', () => {
         cy.visit('http://localhost:5173');
         cy.get('h1[data-cy="title"]').should('have.text', 'Vite + React');
     });
