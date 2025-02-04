dde88ae6cafd741053839c6c07c077f1a7451f46
Scott McFarland <scottm36@gmail.com>
2025-02-04 00:57:08 -0500

    Added another test.

--- cypress/e2e/react-test/todo.cy.ts
+++ cypress/e2e/react-test/todo.cy.ts
@@ -9,6 +9,16 @@ describe('Add new item and verify list', () => {
         cy.get('[data-cy="list-item"]').should('have.length', 5);
         cy.get(':nth-child(5) > .list-item-text').should('have.text', 'Test Item');
     });
-
+    it('should toggle the completed status of an item', () => {
+        cy.visit('http://localhost:5173');
+        cy.get(':nth-child(1) > .list-item-checkbox').click();
+        cy.get(':nth-child(1) > .list-item-text')
+          .should('have.css', 'text-decoration', 'line-through solid rgb(136, 136, 136)');
+    });
+    it('should delete an item from the list', () => {
+        cy.visit('http://localhost:5173');
+        cy.get(':nth-child(1) > .list-item-text').click();
+        cy.get('[data-cy="list-item"]').should('have.length', 4);
+    });
 });
 
