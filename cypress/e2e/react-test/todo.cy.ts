b0ff707825cda92b782a4c0a879dc615ff831d33
Scott McFarland <scottm36@gmail.com>
2025-02-04 00:42:00 -0500

    Corrected a test name.

--- cypress/e2e/react-test/todo.cy.ts
+++ cypress/e2e/react-test/todo.cy.ts
@@ -1,7 +1,7 @@
 import cypress from "cypress";
 
 describe('Add new item and verify list', () => {
-    it('should increment the counter', () => {
+    it('should add a new item to the list and verify the list', () => {
         cy.visit('http://localhost:5173');
         cy.get('[data-cy="list-item"]').should('have.length', 4);
         cy.get('input[data-cy="new-item"]').type('Test Item');
